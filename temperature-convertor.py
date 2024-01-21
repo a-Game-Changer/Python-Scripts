b81edf6313a268032d92696942445be578b9ad16
a-Game-Changer <harshitvarshney822@gmail.com>
2024-01-21 23:31:07 +0530

    temperature-convertor
    - From Celcius to Fahrenheit
    - From Celcius to Kelvin

---
+++ temperature-convertor.py
@@ -0,0 +1,7 @@
+celcius = int(input("Enter the temperature in celcius:"))
+
+fahrenheit = ((9/5)*celcius)+32
+kelvin = celcius+273
+
+print('Temperature in Fahrenheit:', fahrenheit)
+print("Temperature in Kelvin:", kelvin)
